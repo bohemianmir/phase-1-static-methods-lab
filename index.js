6c6a1a6c2ac5e9a91201dc2cde7b2c5f88372cd9
Mira Amer <miraamer@Miras-MacBook-Air.local>
2023-06-26 15:07:17 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
-  //add static methods here
-}
+  
+  
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+  
+    static sanitize(string) {
+      let regex = /[^A-Za-z0-9 '-]/g;
+      return string.replace(regex, "");
+    }
+  
+    static titleize(string) {
+      let words = string.split(" ");
+      let firstWord = words.shift();
+      let titleized = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+      let exceptions = [
+        "the",
+        "a",
+        "an",
+        "but",
+        "of",
+        "and",
+        "for",
+        "at",
+        "by",
+        "from",
+      ];
+      let titleizedWords = words.map((word) => {
+        if (exceptions.includes(word)) {
+          return word;
+        } else {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+      });
+      titleizedWords.unshift(titleized);
+      return titleizedWords.join(" ");
+    }
+  }
